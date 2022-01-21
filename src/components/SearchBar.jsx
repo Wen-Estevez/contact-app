bae5138e23892204ce96da3b09450f05b8419664
Wen-Estevez <westevez04@gmail.com>
2022-01-21 12:48:39 -0500

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { Link } from 'react-router-dom';
+import { searchByName } from '../stateManagement/actions/contactsActions';
+import { useDispatch } from "react-redux";
 //Formulario de busqueda
 
 const SubmitButton = styled.input`    //Boton buscar
@@ -20,11 +22,11 @@ const SubmitButton = styled.input`    //Boton buscar
         color: #fff;
     }
 `;
+
 const TextSearch = styled.input`      //Barra de busqueda
   width:50%;
   height: 1.5rem;
   padding:0.25rem 2rem 0.25rem 0.25rem;
-  border-radius: 15px 0px 0px 15px;
   border: #fff 1px solid;
   opacity: 0.6;
   font-family: "century gothic";
@@ -34,19 +36,40 @@ const TextSearch = styled.input`      //Barra de busqueda
         padding:0.25rem 4rem 0.25rem 0.25rem;
     }
 `;
+const ButtonStyled = styled.button`    //Boton volver
+  width: 7rem;
+  height: 2.1rem;
+  right:0.5rem;
+  border-radius: 15px 0px 0px 15px;
+  color: #000;
+  background: #fff;
+  opacity: 0.7;
+  border: #fff 1px solid ;
+  font-weight: lighter;
+  font-family: "century gothic";
+
+    &:hover{ 
+        background: #010125;
+        color: #fff;
+    }
+`;
 const MenuL = styled(Link)`      //Estilo link
     text-decoration:none;
     margin:0 auto;
 `;
-const ButtonStyled = styled.button`
-`;
+
 export default function SearchBar() {
-    const [contact, setContact] = useState(""); 
-    console.log(contact)
+  const dispatch = useDispatch();
+  const [contact, setContact] = useState(""); 
+  
+  function search(e) {
+    e.preventDefault();
+    dispatch(searchByName(contact));
+  }
+  
       return (
-        <form onSubmit={(e) => {    //Formulario, y funcion de buscar en la app
-          }}>
-                          <MenuL to="/">
+        <form onSubmit={(e) => {search(e)}}>
+            <MenuL to="/">
                 <ButtonStyled>Volver</ButtonStyled>
             </MenuL>
           <TextSearch
@@ -55,7 +78,7 @@ export default function SearchBar() {
                       setContact(e.target.value);
             }}
             type="text"
-            placeholder="¿Busca un contacto?"
+            placeholder="Buscar contactos"
           />
           <SubmitButton type="submit" value="Ver"/> 
         </form>
