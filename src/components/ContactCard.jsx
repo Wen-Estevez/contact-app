a30a58ff2f5cd67bcf64e9f8967203a250d9043c
Wen-Estevez <westevez04@gmail.com>
2022-01-21 09:45:49 -0500

    Update ContactCard.jsx

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { deleteContact } from "../stateManagement/actions/contactsActions";
+import { useDispatch } from "react-redux";
 
 const CardContainer = styled.div`
 
@@ -11,10 +13,19 @@ const TitleText = styled.h1`
 const TextStyled = styled.span`
 
 `;      
+
+const ButtonStyled = styled.button`
+`;
+
 export default function SearchBar({name,phone,date,location,mail}) { 
+    const dispatch = useDispatch();
     
+    function onClose(id) {
+        dispatch(deleteContact(id));
+    }
     return (
         <CardContainer>
+            <ButtonStyled onClick={()=>onClose(name)}>X</ButtonStyled>
             <TitleText>{name}</TitleText>
             <TextStyled>{phone}</TextStyled>
             <TextStyled>{date}</TextStyled>
