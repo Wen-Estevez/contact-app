64da7e429aff8b3dea474ce897b743fb0705dd3b
Wen-Estevez <westevez04@gmail.com>
2022-01-21 00:24:56 -0500

    Update contactsReducer.js

--- src/stateManagement/reducers/contactsReducer.js
+++ src/stateManagement/reducers/contactsReducer.js
@@ -6,10 +6,11 @@ const INITIAL_STATE = {
     contacts:[]          //Inicializa el almacenamiento de contactos vacio
 }
 
-const  contactsReducer = (state=INITIAL_STATE,{type})=>{
+const  contactsReducer = (state=INITIAL_STATE,{type,payload})=>{
     switch  (type){
         case CREATE_CONTACT: return{ 
             ...state,                   //crea un nuevo contacto
+            contacts: state.contacts.concat(payload)
            }
         default: return state;
     }
