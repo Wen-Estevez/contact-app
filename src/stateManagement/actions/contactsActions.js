8496c2001f17f3e2e6baa61e0dec57b2cb001c49
Wen-Estevez <westevez04@gmail.com>
2022-01-21 00:25:21 -0500

    Update contactsActions.js

--- src/stateManagement/actions/contactsActions.js
+++ src/stateManagement/actions/contactsActions.js
@@ -3,7 +3,6 @@ export const CREATE_CONTACT = 'CREATE_CONTACT';
 
 //Acciones del estado de contactos
 
-export function createContact() {
-    return { type: CREATE_CONTACT};
+export function createContact(contact) {
+    return { type: CREATE_CONTACT, payload:contact};
   }
-
