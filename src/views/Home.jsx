839c744bdb004d9b7aa65dd4b0f5d2f54e004390
unknown <westevez04@gmail.com>
2022-01-20 22:54:10 -0500

    tarjetas

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,24 @@
 import styled from "styled-components";
+import crear from "../assets/crear.png";
 
+const TituloStyled = styled.span`
+color: #f1c9e4;
+text-align: center;
+`;
 const ButtonStyled = styled.button`
+    width: 150px;
+    height: 200px;
+    margin: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    display:block;
+    background: #fff;
+    border: 1px #f6f6f6 solid;
+    border-radius: 20px;
+    -webkit-box-shadow: 8px 8px 15px -6px rgba(140,140,140,1);
+    -moz-box-shadow: 8px 8px 15px -6px rgba(140,140,140,1);
+    box-shadow: 8px 8px 15px -6px rgba(140,140,140,1);
+
 `;
 
 const HomeStyled = styled.body`
@@ -10,7 +28,8 @@ export default function Home() {
 
     return (
         <HomeStyled>
-            <ButtonStyled>Crear Contacto</ButtonStyled>
+            <TituloStyled>Agenda de contactos </TituloStyled>
+            <ButtonStyled>Crear Contactos</ButtonStyled>
             <ButtonStyled>Ver Agenda de contactos</ButtonStyled>
         </HomeStyled>
     )
